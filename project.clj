451e74399ad0a748b03a772973e8402d974fb900
Mark Mestern <mark.mestern@uswitch.com>
2021-12-10 13:41:31 +0000

    Update log4j 2.15.0

--- project.clj
+++ project.clj
@@ -6,6 +6,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.1.3"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [log4j/log4j "1.2.17"]
-
+                 [org.apache.logging.log4j/log4j-core "2.15.0"] ;; Warning log4j < 2.15 has vulnerability CVE-2021-44228
                  [ring/ring-mock "0.3.0"]])
