72c102563b6de13af3f2dc307f9d05a757e7a732
Mark Mestern <mark.mestern@uswitch.com>
2021-12-15 15:43:21 +0000

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uswitch/ring-middleware-logging "0.1.5"
+(defproject uswitch/ring-middleware-logging "0.1.6"
   :description "Ring wrappers to log requests and exception to clojure.tools/logging"
   :url "https://github.com/uswitch/ring-middleware-logging"
   :license {:name "Eclipse Public License"
